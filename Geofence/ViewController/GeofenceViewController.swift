d858ce8e6d7206919ae5e2e97d5df16f30299b40
Woon <klwoon1980@yahoo.com>
2019-02-18 21:23:13 +0800

    First commit

---
+++ Geofence/ViewController/GeofenceViewController.swift
@@ -0,0 +1,20 @@
+//
+//  GeofenceViewController.swift
+//  Geofence
+//
+//  Created by Woon on 18/02/2019.
+//  Copyright © 2019 Woon. All rights reserved.
+//
+
+import UIKit
+
+class GeofenceViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ README.md
@@ -0,0 +1,10 @@
+
+Requirements
+------------
+- iOS 12.0+
+- Xcode 10.1+
+- Swift 4.2+
+
+Getting Started
+---------------
+1. Open `Geofence.xcodeproj` with Xcode. 
