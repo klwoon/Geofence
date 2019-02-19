35aa168fb1c46663abc3a1bf89bc6d97eda15964
Woon <klwoon1980@yahoo.com>
2019-02-19 18:03:03 +0800

    Added some helper functions, added MapKit and some user location permission and handling.

---
+++ Geofence/Helper/Utility.swift
@@ -0,0 +1,81 @@
+//
+//  Utility.swift
+//  Geofence
+//
+//  Created by Woon on 19/02/2019.
+//  Copyright © 2019 Woon. All rights reserved.
+//
+
+import UIKit
+
+struct AlertContent {
+    let title: String
+    let message: String
+    let okAction: AlertAction?
+    let cancelAction: AlertAction?
+    
+    init(title: String, message: String, okAction: AlertAction, cancelAction: AlertAction? = nil) {
+        self.title = title
+        self.message = message
+        self.okAction = okAction
+        self.cancelAction = cancelAction
+    }
+}
+
+struct AlertAction {
+    let title: String
+    let action: (() -> Void)?
+    
+    init(title: String, action: (() -> Void)? = nil) {
+        self.title = title
+        self.action = action
+    }
+}
+
+protocol AlertDialogPresenter {
+    func showSingleActionAlert(dialog: AlertContent)
+    func showDoubleActionAlert(dialog: AlertContent)
+}
+
+extension AlertDialogPresenter where Self: UIViewController {
+
+    func showSingleActionAlert(dialog: AlertContent) {
+        let alert = UIAlertController(title: dialog.title,
+                                      message: dialog.message,
+                                      preferredStyle: .alert)
+        let ok = UIAlertAction(title: dialog.okAction?.title,
+                               style: .default,
+                               handler: { _ in dialog.okAction?.action?()})
+        alert.addAction(ok)
+        present(alert, animated: true, completion: nil)
+    }
+
+    func showDoubleActionAlert(dialog: AlertContent) {
+        let alert = UIAlertController(title: dialog.title,
+                                      message: dialog.message,
+                                      preferredStyle: .alert)
+        let ok = UIAlertAction(title: dialog.okAction?.title,
+                               style: .default,
+                               handler: { _ in dialog.okAction?.action?()})
+        let cancel = UIAlertAction(title: dialog.cancelAction?.title,
+                                   style: .default,
+                                   handler: { _ in dialog.cancelAction?.action?()})
+        alert.addAction(ok)
+        alert.addAction(cancel)
+        present(alert, animated: true, completion: nil)
+    }
+}
+
+extension UIApplication {
+    
+    @discardableResult
+    static func openAppSettings() -> Bool {
+        guard
+            let url = URL(string: UIApplication.openSettingsURLString),
+            UIApplication.shared.canOpenURL(url) else {
+                return false
+        }
+        UIApplication.shared.open(url)
+        return true
+    }
+}
--- Geofence/ViewController/GeofenceViewController.swift
+++ Geofence/ViewController/GeofenceViewController.swift
@@ -7,14 +7,67 @@
 //
 
 import UIKit
+import MapKit
 
 class GeofenceViewController: UIViewController {
 
+    @IBOutlet weak var mapView: MKMapView!
+    
+    var locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        setupMapView()
     }
 
+    private func setupMapView() {
+        locationManager.delegate = self
+        locationManager.requestAlwaysAuthorization()
+    }
+    
+    func zoom(to coordinate: CLLocationCoordinate2D? = nil) {
+        let location = coordinate ?? mapView.userLocation.coordinate
+        let region = MKCoordinateRegion(center: location, latitudinalMeters: 10000, longitudinalMeters: 10000)
+        mapView.setRegion(region, animated: true)
+    }
 
 }
 
+extension GeofenceViewController: CLLocationManagerDelegate {
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        switch status {
+        case .authorizedAlways, .authorizedWhenInUse:
+            mapView.showsUserLocation = true
+            locationManager.requestLocation()
+            
+        case .denied:
+            let alert = AlertContent(title: "Permission Denied",
+                                     message: "Please go to Settings to enable Location.",
+                                     okAction: AlertAction(title: "Setting",
+                                                           action: { UIApplication.openAppSettings() }),
+                                     cancelAction: AlertAction(title: "Cancel"))
+            showDoubleActionAlert(dialog: alert)
+            
+        default:
+            break
+            
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        zoom(to: locations.first?.coordinate)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        showSingleActionAlert(dialog: AlertContent(title: "Error",
+                                                   message: error.localizedDescription,
+                                                   okAction: AlertAction(title: "OK")))
+    }
+}
+
+extension GeofenceViewController: AlertDialogPresenter {
+
+}
